bae138edc5ac92b04d595e69a01aad1ed023f3b3
Aleksandar Zivanovic <56928480+aleksandar-zivanovic@users.noreply.github.com>
2021-07-27 17:12:45 +0200

    added includes/config.php

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/* email */
+const SMTP_HOST = 'enter your smtp host';
+const SMTP_PORT = 'enter your smtp port';
+const SMTP_USER = 'enter your smtp user';
+const SMTP_PASSWORD = 'enter your smtp passwhord';
+
+const FROM_EMAIL = 'enter your email ';
+const FROM_NAME = 'enter email's from';
